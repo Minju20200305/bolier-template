dcee4a84dc89e993f916dad9a1f17ec877312750
minju20200305 <minju.kwon.0417@gmail.com>
2021-12-19 18:37:12 +0900

    13강

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
 const config = require('./config/key')
-
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User')
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,31 +21,27 @@ res.send('Hello World!')
 })
 
 
-app.post('/register', (req, res) =>{
+app.post('/api/users/register', (req, res) =>{
 //회원가입에 필요한 정보들을 클라이언트에서 가져오면
 //그것을 데이터 베이스에 넣어준다
 
     const user = new User(req.body)
     user.save((err, doc) => {
-
         if(err) return res.json({success: false, err})
-    
         return res.status(200).json({
             success: true
         })
         })
-
 })
 
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 //요청된 이메일은 데이터 베이스에서 찾는다
 User.findOne({ email: req.body.email }, (err, user) =>{
     if(!user) {
         return res.json({
             loginSuccess: false,
             message: "제공된 이메일에 해당하는 유저가 없습니다"
-
         })
     }
     user.comparePassword(req.body.password, (err, isMatch) => {
@@ -62,13 +57,24 @@ User.findOne({ email: req.body.email }, (err, user) =>{
             .json({ loginSuccess: true, userId: user._id})
             
         })
-
-
 })
 })
-
 })
 //비밀번호까지 맞다면 토큰 생성.
+app.get('/api/users/auth', auth, (req,res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentification이 true인 경우
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role == 0 ? false : true,
+        isAuth: true, 
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+
+})
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -76,8 +76,20 @@ userSchema.methods.generateToken = function(cb){
         if(err) return cb(err)
         cb(null, user)
     })
+}
 
-
+userSchema.statics.findByTokwn = function(token, cd){
+    let user = this;
+    //토큰을 디코드 하는 법
+    user._id + '' = token;
+    jwt.verify(token,'secretToken', function(err, decoded){
+        //유저 아이드를 이용해서 유저를 찾은 다음에 
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id: decoded", "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
 }
 const User = mongoose.model('User', userSchema);
 module.exports = { User }
